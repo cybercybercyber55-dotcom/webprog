c61c7bbae3bef8459457082fac7a326ca5576d0c
cybercybercyber55-dotcom <cybercybercyber55@gmail.com>
2025-12-13 20:53:33 +0800

    Refactor import statement for website module

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import os
 # Add project root to PYTHONPATH
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from webprog.website import create_app
+from website import create_app
 
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
 
