b8181679f943f6eacdaac9fa7af6e63176a0c5cd
cybercybercyber55-dotcom <cybercybercyber55@gmail.com>
2025-12-13 20:42:17 +0800

    Update import path for create_app function

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 # Add project root to PYTHONPATH
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from website import create_app
+from webprog.website import create_app
+
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
